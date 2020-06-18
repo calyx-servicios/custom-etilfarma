e25132ef6779a7b7f2a0bfe771be45a1ec13b391
Jhone Mendez <jhone0901@gmail.com>
2020-06-18 19:54:26 -0300

    [NEW] [purchase_foreing_lines] new packaging models, fix views, add new field in purchase order type, change .py field names

--- foreign_purchase_order/models/purchase_order.py
+++ foreign_purchase_order/models/purchase_order.py
@@ -12,33 +12,52 @@ class PurchaseOrder(models.Model):
         # related="order_type.name"
     )
 
+    order_type_blanket = fields.Boolean(string="Blanket", default=False)
+
+    purchase_sample = fields.Boolean(string="Sample?")
+
     customer_purchase_order = fields.Char(string="Customer Purchase Order")
     place_of_delivery_id = fields.Many2one(
-        comodel_name='purchase.delivery' ,string="Place of Delivery")
+        comodel_name="purchase.delivery", string="Place of Delivery"
+    )
     packaging_id = fields.Many2one(
-        comodel_name='purchase.packaging' ,string="Packaging")
-    
+        comodel_name="purchase.packaging", string="Packaging"
+    )
 
     @api.onchange("order_type")
     def _onchange_order_type(self):
         """
-        When the order types changes, we search the order type OCE id
-        and compares with the order type seleted by the user
-        if is the same then set the order_type_name to 'OCE'
-        this is for show some fields in the purchase form view
-        if not then clean the values of customer_purchase_order and
-        order_type_name.
+        When the order type changes, check if that order is a blanket order
+        if it is, then set like True the variable order_type_blanket to show some fields in
+        the purchase view.
         """
-        oce = self.env.ref("purchase_order_types.po_type_blanket")
+        # oce = self.env.ref("purchase_order_types.po_type_blanket")
         for record in self:
             if record.order_type:
-                if record.order_type.id == oce.id:
-                    record.order_type_name = "OCE"
+                # if record.order_type.id == oce.id:
+                # record.order_type_name = "OCE"
+                if record.order_type.blanket:
+                    record.order_type_blanket = True
                 else:
                     record.customer_purchase_order = ""
-                    record.order_type_name = ""
+                    record.order_type_blanket = False
+
+                    # record.order_type_name = ""
             else:
                 record.customer_purchase_order = ""
-                record.order_type_name = ""
+                record.order_type_blanket = False
 
+                # record.order_type_name = ""
 
+    @api.onchange("purchase_sample")
+    def _onchange_purchase_sample(self):
+        """
+         Set the order type in OCM
+        """
+        ocm = self.env.ref("purchase_order_types.po_type_sample")
+        for record in self:
+            if ocm:
+                record.order_type = ocm.id
+                if ocm.blanket:
+                    record.order_type_blanket = True
+                # record.order_type_name = "OCM"
