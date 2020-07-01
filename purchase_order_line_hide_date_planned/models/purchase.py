f5adb061b47b919dd8dcd3b4f90e9d0b414bc6c0
Lucas L. Soto <4183046-sotolucas@users.noreply.gitlab.com>
2020-07-01 15:30:10 -0300

    [FIX] purchase_order_line_hide_date_planned
     - Improve how methods are inherited to avoid conflicts with other modules.

--- purchase_order_line_hide_date_planned/models/purchase.py
+++ purchase_order_line_hide_date_planned/models/purchase.py
@@ -2,7 +2,8 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import models, fields, api
-
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
+from datetime import datetime
 
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
@@ -16,7 +17,7 @@ class PurchaseOrder(models.Model):
 
     @api.multi
     @api.onchange("date_planned")
-    def onchange_date_planned(self):
+    def _onchange_date_planned(self):
         """Make the PO lines take in count this value as it changes"""
         for order in self:
             order.order_line.update({"date_planned": order.date_planned})
@@ -29,50 +30,20 @@ class PurchaseOrderLine(models.Model):
 
     @api.onchange("product_qty", "product_uom")
     def _onchange_quantity(self):
-        """Override native method to force the PO line date_planned field to be
-        the order's one. TO-DO: Do it smoothly"""
-        if not self.product_id:
-            return
-
-        seller = self.product_id._select_seller(
-            partner_id=self.partner_id,
-            quantity=self.product_qty,
-            date=self.order_id.date_order and self.order_id.date_order[:10],
-            uom_id=self.product_uom,
-        )
-
-        self.date_planned = self.order_id.date_planned
-
-        if not seller:
-            return
-
-        price_unit = (
-            self.env["account.tax"]._fix_tax_included_price_company(
-                seller.price,
-                self.product_id.supplier_taxes_id,
-                self.taxes_id,
-                self.company_id,
-            )
-            if seller
-            else 0.0
-        )
-        if (
-            price_unit
-            and seller
-            and self.order_id.currency_id
-            and seller.currency_id != self.order_id.currency_id
-        ):
-            price_unit = seller.currency_id.compute(
-                price_unit, self.order_id.currency_id
-            )
-
-        if seller and self.product_uom and seller.product_uom != self.product_uom:
-            price_unit = seller.product_uom._compute_price(price_unit, self.product_uom)
-
-        self.price_unit = price_unit
+        """Inherit native method to force the PO line date_planned field to be
+        the order's one."""
+        for line in self:
+            self.date_planned = self.order_id.date_planned
+        return super(PurchaseOrderLine, self)._onchange_quantity()
 
     @api.model
-    def _get_date_planned(self):
+    def _get_date_planned(self, seller, po=False):
         """Override native method to force the PO line date_planned field to be
         the order's one."""
-        return self.order_id.date_planned
+        if self.order_id.date_planned:
+            return datetime.strptime(
+                self.order_id.date_planned,
+                DEFAULT_SERVER_DATETIME_FORMAT
+            )
+        else:
+            return datetime.today()
