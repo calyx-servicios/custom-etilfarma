4e12e7ba2a3c0b7290772b4acb17401f1d30c7eb
FedericoGregori <jfedericogregori@gmail.com>
2020-08-28 02:39:12 -0300

    [ADD][foreign_purchase_order] Invoice same currency as PO

--- foreign_purchase_order/models/__init__.py
+++ foreign_purchase_order/models/__init__.py
@@ -6,3 +6,4 @@ from . import purchase_packing_list
 from . import res_config_settings
 from . import purchase_certificate_of_analysis
 from . import purchase_import_license
+from . import account_invoice
