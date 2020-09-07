29a78f26a687c347339f85bb12668b9eb734489d
paradisocrist <crisparadiso_@hotmail.com>
2020-09-07 10:28:03 -0300

    [add][datetime_without_time] Change all datetime fields to date only in the view

---
+++ datetime_without_time/__manifest__.py
@@ -0,0 +1,38 @@
+{
+    'name': 'Custom Etilfarma Datetime Without Time',
+    'version': '11.0.1',
+    'category': 'Tools',
+    'author': "Calyx",
+    'website': 'www.calyxservicios.com.ar',
+    'license': 'AGPL-3',
+    'summary': '''Change all datetime fields to date only in the following views:
+                views/purchase_order_view.xml
+                views/purchase_request_view.xml
+                views/sale_order_view.xml
+                views/stock_inventory_view.xml
+                views/stock_move_line_view.xml
+                views/stock_picking_view.xml
+                views/stock_production_lot_view.xml
+                views/stock_quant_view.xml                                                    ''',
+    'depends': [
+    ],
+    'external_dependencies': {
+    },
+    'data': [
+        'views/purchase_order_view.xml',
+        'views/purchase_request_view.xml',
+        'views/sale_order_view.xml',
+        'views/stock_inventory_view.xml',
+        'views/stock_move_line_view.xml',
+        'views/stock_picking_view.xml',
+        'views/stock_production_lot_view.xml',
+        'views/stock_quant_view.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
