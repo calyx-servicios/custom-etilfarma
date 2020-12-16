03823a9fc55b09ebd65c91f26fd5eaf18dda04cb
Jhone <jhone0901@gmail.com>
2020-12-16 14:57:01 -0300

    [NEW] [l10n_aeroo_einvoice_custom_output] ODOO-283 (#22)
    * [NEW] [l10n_aeroo_einvoice_custom_output]
    Incidence nbr./s.: ODOO-283.
     - New .odt template file to einvoice report.
    
    * Add init in models
    
    * [UPD][l10n_aeroo_einvoice_custom_output]
    Incidence nbr./s: ODOO-283:
    - Add sale_order_id m2o field to ['account.invoice'].
    - Update the .odt template.
    - Update the manifest version.

---
+++ l10n_aeroo_einvoice_custom_output/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Einvoice custom Output",
+    "summary": """
+        Custom Output in einvoice""",
+    "author": "Calyx Servicios S.A.",
+    "maintainers": ["JhoneM"],
+    "website": "http://odoo.calyx-cloud.com.ar/",
+    "license": "AGPL-3",
+    "category": "Report",
+    "version": "11.0.1.1.1",
+    "development_status": "Production/Stable",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {"python": [], "bin": []},
+    "depends": ["l10n_ar_afipws_fe", "l10n_ar_aeroo_einvoice"],
+    "data": ["report/invoice_report.xml"],
+}
