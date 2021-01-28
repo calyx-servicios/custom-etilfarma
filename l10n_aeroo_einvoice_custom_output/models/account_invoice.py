226a83b3489eb99e1fe00366c6bce549f547dc93
JhoneM <jhone0901@gmail.com>
2021-01-28 17:41:21 -0300

    [UPD][l10n_aeroo_einvoice_custom_output] Incidence nrb.s/: ODOO-895 - New report function in [account_invoice]. -  Update manifest file. - Conversor.py number to word in english. - New .odt english report template. - Views and translations. - Requirements file.

--- l10n_aeroo_einvoice_custom_output/models/account_invoice.py
+++ l10n_aeroo_einvoice_custom_output/models/account_invoice.py
@@ -1,6 +1,7 @@
 # Copyright 2020 Calyx Servicios S.A.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from odoo import models, fields, api
+from . import conversor
 
 
 class AccountInvoice(models.Model):
@@ -18,3 +19,19 @@ class AccountInvoice(models.Model):
     sale_order_id = fields.Many2one(
         "sale.order", string="Sale order", compute="_get_sale_id"
     )
+
+    @api.multi
+    def invoice_en_print(self):
+        """
+        Print the Einvoice custom report and mark it as sent.
+        """
+        self.sent = True
+        return self.env.ref(
+            "l10n_aeroo_einvoice_custom_output.action_aeroo_report_ar_einvoice_en"
+        ).report_action(self)
+
+    @api.multi
+    def number2word_en(self, number):
+        num = str(number)
+        word = conversor.dollars2words(num)
+        return word.capitalize()
