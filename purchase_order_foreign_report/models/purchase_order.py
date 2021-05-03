c7499fe527dd3aa8677efa0e73ccf34731979be4
JhoneM <jhone0901@gmail.com>
2021-05-03 15:48:19 -0300

    [UPD][purchase_order_foreign_report] Incidence nrb./s.: ODOO-1379:  - Upd report template.  - Add less file to add fonts.  - Add fonts (.tff) files.  - Upd Manifest.

--- purchase_order_foreign_report/models/purchase_order.py
+++ purchase_order_foreign_report/models/purchase_order.py
@@ -69,14 +69,14 @@ class PurchaseOrder(models.Model):
 
     def get_date_format_english(self):
         """
-            Get the date of the order 
-            and return in english format
+        Get the date of the order
+        and return in english format
         """
         for order in self:
             locale.setlocale(locale.LC_TIME, "en_US.UTF-8")
             date = order.date_order
-            city = str(order.company_id.state_id.name)
+            # city = str(order.company_id.state_id.name)
             date = dateutil.parser.parse(date).date()
             new_date = date.strftime("%A, %B %dth, %Y")
-            new_format_date = city + ", " + new_date
+            new_format_date = new_date
             return new_format_date
