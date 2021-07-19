5c63f83d22b2c6c4d856ac1cf4fa79de9d7ac7c5
marcooegg <marco.oegg@gmail.com>
2021-07-19 11:56:28 -0300

    funciona

--- stock_move_lines/models/stock_move_line.py
+++ stock_move_lines/models/stock_move_line.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api, http
 
 class StockPicking(models.Model):
     _inherit = "stock.move.line"
@@ -40,4 +40,24 @@ class StockPicking(models.Model):
         ('internal', 'Internal')], 
         related='picking_id.picking_type_id.code',
         readonly=True)
-        
+
+
+    @api.multi
+    def get_link(self):
+        def get_move(records,origin):
+            for line in records:
+                if line.name == origin:
+                    return line.id
+        base_url = http.request.env['ir.config_parameter'].get_param('web.base.url')
+        action_id = self.env.ref('sale.action_quotations')
+        menu_id = self.env.ref('sale.menu_sale_quotations')
+        move_ids = self.env['sale.order'].search([])
+        for line in self:
+            move_id = get_move(move_ids,line.origin)
+            line.url = '%s/web#id=%s&view_type=form&model=sale.order&menu_id=%s&action=%s'%(base_url,move_id,menu_id.id,action_id.id)
+
+    
+    url = fields.Char(
+        'url',
+        compute = 'get_link'
+    )
