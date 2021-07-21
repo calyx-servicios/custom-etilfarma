b0ce218f940a3c8d639a7d00d8d2394ec43806a7
marcooegg <marco.oegg@gmail.com>
2021-07-21 19:10:56 -0300

    ESTAMOS

--- stock_move_lines/models/stock_move_line.py
+++ stock_move_lines/models/stock_move_line.py
@@ -57,16 +57,69 @@ class StockPicking(models.Model):
         sales_action_id = self.env.ref('sale.action_quotations')
         sales_menu_id = self.env.ref('sale.menu_sale_quotations')
         sales_move_ids = self.env['sale.order'].search([])
+        #Get Sale and Purchase sequence prefix
+        sales_prefix = self.env['ir.sequence'].search([('sale_seq','=',True)])
+        purchase_prefix = self.env['ir.sequence'].search([('purchase_seq','=',True)])
         for line in self:
             if "SO" in line.origin:
                 move_id = get_move(sales_move_ids,line.origin)
                 line.url = '%s/web#id=%s&view_type=form&model=sale.order&menu_id=%s&action=%s'%(base_url,move_id,sales_menu_id.id,sales_action_id.id)
+                #line.url = 
             elif "OC" or "p" in line.origin:
                 move_id = get_move(purchase_move_ids,line.origin)
-                line.url = '%s/web#id=%s&view_type=form&model=purchase.order&menu_id=%s&action=%s'%(base_url,move_id,purchase_menu_id.id,purchase_action_id.id)     
+                #line.url = '%s/web#id=%s&view_type=form&model=purchase.order&menu_id=%s&action=%s'%(base_url,move_id,purchase_menu_id.id,purchase_action_id.id)     
 
 
-    
+    def get_url(self):
+        base_url = base_url = http.request.env['ir.config_parameter'].get_param('web.base.url')
+        sales_action_id = self.env.ref('sale.action_quotations')
+        sales_move_id = self.env['sale.order'].search([('name','=',self.origin)])
+        sales_menu_id = self.env.ref('sale.menu_sale_quotations')
+        purchase_action_id = self.env.ref('purchase.purchase_form_action')
+        purchase_menu_id = self.env.ref('purchase.menu_purchase_form_action')
+        purchase_move_id = self.env['purchase.order'].search([('name','=',self.origin)])
+        if "SO" in self.origin:
+            #move_id = get_move(sales_move_ids,self.origin)
+            return ('%s/web#id=%s&view_type=form&model=sale.order&menu_id=%s&action=%s'%(base_url,sales_move_id,sales_menu_id.id,sales_action_id.id))
+            #self.url = 
+        elif "OC" or "p" in self.origin:
+            #move_id = get_move(purchase_move_ids,self.origin)
+            return ('%s/web#id=%s&view_type=form&model=purchase.order&menu_id=%s&action=%s'%(base_url,purchase_move_id,purchase_menu_id.id,purchase_action_id.id))
+
+    @api.depends('origin')
+    def get_link_action(self):
+        base_url = base_url = http.request.env['ir.config_parameter'].get_param('web.base.url')
+        
+        sales_prefix_ids = self.env['ir.sequence'].search([('sale_seq','=',True)])
+        sale_prefixes = []
+        for x in sales_prefix_ids:
+            sale_prefixes.append(x.prefix)
+        
+        purchase_prefix_ids = self.env['ir.sequence'].search([('purchase_seq','=',True)])
+        purchase_prefixes = []
+        for x in purchase_prefix_ids:
+            purchase_prefixes.append(x.prefix)
+        
+        if any(prefix in self.origin for prefix in sale_prefixes):
+            sales_action_id = self.env.ref('sale.action_quotations')
+            sales_move_id = self.env['sale.order'].search([('name','=',self.origin)])
+            return {
+                "type": "ir.actions.act_url",
+                "url" : "%s/web#id=%s&view_type=form&model=sale.order&action=%s" % 
+                (base_url,sales_move_id.id,sales_action_id.id),
+                "target" : "new"
+            }
+            
+        elif any(prefix in self.origin for prefix in purchase_prefixes):
+            purchase_action_id = self.env.ref('purchase.purchase_form_action')
+            purchase_move_id = self.env['purchase.order'].search([('name','=',self.origin)])
+            return {
+                "type": "ir.actions.act_url",
+                "url" : "%s/web#id=%s&view_type=form&model=purchase.order&action=%s" % 
+                (base_url,purchase_move_id.id,purchase_action_id.id),
+                "target" : "new"
+            }
+
     url = fields.Char(
         'url',
         compute = 'get_link'
