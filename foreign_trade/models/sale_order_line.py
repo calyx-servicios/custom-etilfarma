7c63c853aa0a37dd361eb5c447b0176bdd607b44
Andres Andrade <andres.63.d@gmail.com>
2021-10-27 14:21:31 -0300

    [UPD][import_dispatch][foreign_trade] Incidence nbr./s.: ODOO-1818.  - Relate fields lot, dispatch and expiration date, from the delivery order to the sales order

--- foreign_trade/models/sale_order_line.py
+++ foreign_trade/models/sale_order_line.py
@@ -102,34 +102,51 @@ class SaleOrderLine(models.Model):
 
     @api.onchange("product_id")
     def _get_lot_by_product(self):
-            names = []
-            filter_ids = []
-            lot_ids = self.env["stock.production.lot"].search([("product_id", "=", self.product_id.id)])
-            for lot_id in lot_ids:
-                if lot_id.name not in names:
-                    filter_ids.append(lot_id.id)
-                    names.append(lot_id.name)
+        # if self.product_id:
+            # import wdb
+            # wdb.set_trace()
+        names = []
+        filter_ids = []
+        lot_ids = self.env["stock.production.lot"].search([("product_id", "=", self.product_id.id)])
+        for lot_id in lot_ids:
+            if lot_id.name not in names:
+                filter_ids.append(lot_id.id)
+                names.append(lot_id.name)
                     
-            self.lot_filter = [(6,0, filter_ids)]
+        self.lot_filter = [(6,0, filter_ids)]
 
     lot_filter = fields.Many2many(
         'stock.production.lot')
     
     @api.onchange("loot_name")
     def _get_dispatch_by_product(self):
-            names = []
-            filter_ids = []
-            dispatch_ids = self.env["stock.production.dispatch"].search([("product_id", "=", self.product_id.id)])
-            for dispatch_id in dispatch_ids:
-                if dispatch_id.name not in names:
-                    #  and dispatch_id.lot_id.id == self.loot_name.dispatch_id.id
-                    filter_ids.append(dispatch_id.id)
-                    names.append(dispatch_id.name)
+        names = []
+        filter_ids = []
+        for rec in self:
+            if rec.loot_name:
+                # import wdb
+                # wdb.set_trace()
+                dispatch_ids = self.env["stock.production.dispatch"].search([("product_id", "=", rec.product_id.id),("lot_id", "=", rec.loot_name.id)])
+                lot_ids = self.env["stock.production.lot"].search([("product_id", "=", rec.product_id.id),("name", "=", rec.loot_name.name)])
+                for dispatch_id in dispatch_ids:
+                    for lot_id in lot_ids:
+                        if dispatch_id.id == lot_id.dispatch_id.id: 
+                            if dispatch_id.name not in names:
+                                filter_ids.append(dispatch_id.id)
+                                names.append(dispatch_id.name)
                     
-            self.dispatch_filter = [(6,0, filter_ids)]
+                    
+            rec.dispatch_filter = [(6,0, filter_ids)]
 
     dispatch_filter = fields.Many2many(
         'stock.production.lot')
+
+    @api.onchange("line_dispatch_name")
+    def _get_lot_by_dispatch(self):
+        # import wdb
+        # wdb.set_trace()
+        if self.loot_name and self.line_dispatch_name:
+            self.loot_name = self.line_dispatch_name.lot_id
 
     product_nmc = fields.Char(string="HS Code", related="product_id.product_nmc")
     country_id = fields.Char(string="Origin", required=True, related="product_tmpl_id.country_id.name")
