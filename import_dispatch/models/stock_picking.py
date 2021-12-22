b35057cf2f318280bf27d743d795efbb486a0954
DarwinAndrade <andres.63.d@gmail.com>
2021-12-22 11:28:58 -0300

    [FIX][import_dispatch] Incidence nbr./s.: ODOO-1860.  - Modify the function of validate in stock picking

--- import_dispatch/models/stock_picking.py
+++ import_dispatch/models/stock_picking.py
@@ -13,7 +13,7 @@ class StockPicking(models.Model):
         string='Dispatch Name',
     )
 
-    @api.multi
+@api.multi
     def button_validate(self):
         res = super(StockPicking, self).button_validate() 
         if self.picking_type_code == 'incoming':
@@ -32,15 +32,27 @@ class StockPicking(models.Model):
                     if lot_id.dispatch_id.name == move.dispatch_name:
                         raise ValidationError(_('The combination of serial number and product must be unique !'))               
                 lot_name =  self.env['stock.production.lot'].search([('name','=',move.lot_name),('product_id','=', move.product_id.id),('dispatch_id','=', False)])
-                lot_name.write({
+                if not lot_name:
+                    lot_name = self.env['stock.production.lot'].create({
+                    'name': move.lot_name,
+                    'product_id': move.product_id.id,
+                    'product_uom_id':move.product_uom_id.id,
+                    'product_qty':move.qty_done,
                     'dispatch_id': rec.id,
                     'life_date': move.move_id.editable_life_date,
-                })
+                    })
+                else:
+                    lot_name.write({
+                        'dispatch_id': rec.id,
+                        'life_date': move.move_id.editable_life_date,
+                    })        
+                move.dispatch_id = rec.id
+                move.lot_id = lot_name.id
         if self.picking_type_code == 'outgoing':                   
             for move in self.move_lines:
                 dispatch = self.env['stock.production.dispatch'].search([('name','=',move.dispatch_name),('product_id','=', move.product_id.id)])
                 if dispatch:
                     dispatch.write({'product_qty': dispatch.product_qty - move.qty_done})
 
-        return res             
+        return res         
             
