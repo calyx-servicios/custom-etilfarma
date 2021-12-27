7a0177fc0b0d58901b52be5b8e621f5081ae93c1
DarwinAndrade <andres.63.d@gmail.com>
2021-12-27 10:08:51 -0300

    upd

--- stock_picking_lot_extension/models/stock_picking.py
+++ stock_picking_lot_extension/models/stock_picking.py
@@ -8,20 +8,20 @@ from odoo.exceptions import UserError
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    @api.multi
-    def button_validate(self):
-        order_type = self.origin
-        lines_to_check = self.move_lines
-        if order_type:
-            if order_type[0:3] == "OCM" or order_type[0:3] == "OCE":
-                record = self.env['purchase.order'].search([('name', '=', order_type)])
-                for line in lines_to_check:
-                    product = line.product_id
-                    if product:
-                        if not line.editable_life_date:
-                            raise UserError(_('You need to supply a life date for %s.') % product.display_name)      
-        res = super(StockPicking, self).button_validate() 
-        return res           
+    # @api.multi
+    # def button_validate(self):
+    #     order_type = self.origin
+    #     lines_to_check = self.move_lines
+    #     if order_type:
+    #         if order_type[0:3] == "OCM" or order_type[0:3] == "OCE":
+    #             record = self.env['purchase.order'].search([('name', '=', order_type)])
+    #             for line in lines_to_check:
+    #                 product = line.product_id
+    #                 if product:
+    #                     if not line.editable_life_date:
+    #                         raise UserError(_('You need to supply a life date for %s.') % product.display_name)      
+    #     res = super(StockPicking, self).button_validate() 
+    #     return res           
             
     @api.multi
     def button_validate(self):
