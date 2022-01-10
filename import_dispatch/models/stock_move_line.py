fc0ad0c9f6eabc9bd197f7d2a17495d593c7ca95
DarwinAndrade <andres.63.d@gmail.com>
2022-01-10 10:00:02 -0300

    Revert "Merge pull request #118 from calyx-servicios/11.0_fix_import_dispatch"
    This reverts commit 0df4bb0fed68f1f2a0e8b394debee23fc5294247, reversing
    changes made to 7de1bdb90f5120c7dbe724e8c578bfcd5731f3d4.

--- import_dispatch/models/stock_move_line.py
+++ import_dispatch/models/stock_move_line.py
@@ -4,7 +4,6 @@
 from odoo import api, fields, models, _
 from odoo.tools.float_utils import float_compare
 from odoo.exceptions import UserError
-from openerp.tools.translate import _
 
 class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
@@ -15,54 +14,8 @@ class StockMoveLine(models.Model):
     dispatch_name = fields.Char(
         string='Dispatch Name',
     )
-    life_date = fields.Date(
-        string='Life Date',
-    )
-    lot_id = fields.Many2one(
-        string='lote prueba'
-    )
     @api.onchange('lot_name')
     def onchange_document_number(self):
         for rec in self.picking_id.move_lines:
             if self.product_id.id == rec.product_id.id and rec.dispatch_name and not self.dispatch_name:
                 self.dispatch_name = rec.dispatch_name
-
-    @api.onchange('dispatch_id')
-    def onchange_dispatch_id(self):
-        self.life_date = self.dispatch_id.lot_id.life_date
-        
-    @api.onchange("product_id")
-    def _get_lot_by_product(self):
-        names = []
-        filter_ids = []
-        lot_ids = self.env["stock.production.lot"].search([("product_id", "=", self.product_id.id)])
-        for lot_id in lot_ids:
-            if lot_id.name not in names:
-                filter_ids.append(lot_id.id)
-                names.append(lot_id.name)
-                    
-        self.lot_filter = [(6,0, filter_ids)]
-
-    lot_filter = fields.Many2many(
-        'stock.production.lot')
-        
-    @api.onchange("lot_id")
-    def _get_dispatch_by_product(self):
-        names = []
-        filter_ids = []
-        for rec in self:
-            if rec.lot_id:
-                dispatch_ids = self.env["stock.production.dispatch"].search([("product_id", "=", rec.product_id.id),("lot_id", "=", rec.lot_id.id)])
-                lot_ids = self.env["stock.production.lot"].search([("product_id", "=", rec.product_id.id),("name", "=", rec.lot_id.name)])
-                for dispatch_id in dispatch_ids:
-                    for lot_id in lot_ids:
-                        if dispatch_id.id == lot_id.dispatch_id.id: 
-                            if dispatch_id.name not in names:
-                                filter_ids.append(dispatch_id.id)
-                                names.append(dispatch_id.name)
-                    
-                    
-            rec.dispatch_filter = [(6,0, filter_ids)]
-
-    dispatch_filter = fields.Many2many(
-        'stock.production.lot')
