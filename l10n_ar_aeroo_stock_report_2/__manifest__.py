effbf4ac7141489bcfd2f80f5815e3ded1bd70c3
DarwinAndrade <andres.63.d@gmail.com>
2022-02-21 16:05:07 -0300

    upd

--- l10n_ar_aeroo_stock_report_2/__manifest__.py
+++ l10n_ar_aeroo_stock_report_2/__manifest__.py
@@ -1,6 +1,6 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Aeroo Stock report',
+    'name': 'Preprinted Report',
     'summary': """Add the variant, lot, origin, dispatch and manufacturer fields to the shipment report""",
 
     'author': 'Calyx Servicios S.A., Odoo Community Association (OCA)',
