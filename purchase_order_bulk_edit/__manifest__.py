e63a73d7c54e8ae674fa81fd72f84bed1a741e3e
deykerag <deykergil28@gmail.com>
2022-09-15 19:03:33 -0400

    [ODOO-4648][UPD][purchase_order_bulk_edit] 	- Add menu for wizard view 	- Security

--- purchase_order_bulk_edit/__manifest__.py
+++ purchase_order_bulk_edit/__manifest__.py
@@ -18,6 +18,8 @@
     },
     "depends": ["purchase"],
     "data": [
+        'security/ir.model.access.csv',
         "wizard/purchase_order_bulk_edit.xml",
+        "wizard/menu_bulk_edit.xml",
     ],
 }
