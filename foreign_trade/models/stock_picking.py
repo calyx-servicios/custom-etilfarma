e9f28c114648ffa3a96a03b9c0f849ca13a56c9a
deykerag <deykergil28@gmail.com>
2022-09-21 09:43:43 -0400

    [ODOO-4677][UPD][foreign_trade] 	- Secuence to field in report

--- foreign_trade/models/stock_picking.py
+++ foreign_trade/models/stock_picking.py
@@ -43,6 +43,18 @@ class StockPicking(models.Model):
         string="Container"
     )
 
+    seq = fields.Char(
+        string="Sequence",
+        required=True,
+    )
+
+    @api.model
+    def create(self, vals):
+        if not vals.get('seq'):
+            vals['seq'] = self.env['ir.sequence'].next_by_code(
+                'stock.picking')
+
+
 class StockMove(models.Model):
     _inherit = "stock.move"
 
