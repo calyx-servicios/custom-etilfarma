743da6c8f21515acc6d3a48d5593a7eee5dc7eba
lucianobaleani <lucianobaleani94@gmail.com>
2022-10-20 20:12:30 -0300

    [ODOO-4827][ADD][stock_lot_dispatch]

---
+++ stock_lot_dispatch/__manifest__.py
@@ -0,0 +1,21 @@
+# pylint: disable=missing-module-docstring,pointless-statement
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock's Lot & Dispatch",
+    "summary": """
+        This module allow the visualization of the dispatch, lot and life date of the stocked products
+    """,
+    "author": "Calyx Servicios S.A.",
+    "maintainers": ["lucianobaleani"],
+    "website": "https://odoo.calyx-cloud.com.ar/",
+    "license": "AGPL-3",
+    "category": "Stock",
+    "version": "11.0.1.0.0",
+    "development_status": "Production/Stable",
+    "application": False,
+    "installable": True,
+    "depends": ["stock", "import_dispatch"],
+    'data': [
+        "views/stock_picking_inherited_views.xml"
+    ],
+}
